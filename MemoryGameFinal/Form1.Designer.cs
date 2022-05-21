27dfc46524c187c7150713e920d98c1a73e8a489
Vasil <33176648+kokonut79@users.noreply.github.com>
2022-05-22 00:01:04 +0300

    Add files via upload

---
+++ MemoryGameFinal/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MemoryGameFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lStratus = new System.Windows.Forms.Label();
+            this.bRestart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lStratus
+            // 
+            this.lStratus.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lStratus.Location = new System.Drawing.Point(241, 70);
+            this.lStratus.Name = "lStratus";
+            this.lStratus.Size = new System.Drawing.Size(178, 84);
+            this.lStratus.TabIndex = 0;
+            this.lStratus.Text = "Memory Game By Vasil Parshev";
+            this.lStratus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lStratus.Click += new System.EventHandler(this.lStratus_Click);
+            // 
+            // bRestart
+            // 
+            this.bRestart.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bRestart.Location = new System.Drawing.Point(274, 22);
+            this.bRestart.Name = "bRestart";
+            this.bRestart.Size = new System.Drawing.Size(114, 45);
+            this.bRestart.TabIndex = 1;
+            this.bRestart.Text = "Restart";
+            this.bRestart.UseVisualStyleBackColor = true;
+            this.bRestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 273);
+            this.Controls.Add(this.bRestart);
+            this.Controls.Add(this.lStratus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lStratus;
+        private System.Windows.Forms.Button bRestart;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
---
+++ MemoryGameFinal/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGameFinal
+{
+    public partial class Form1 : Form
+    {
+
+        List<int> numbers = new List<int>{1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8};
+        private string firstClicked;
+        private string secondClicked;
+       
+        List<PictureBox> pictures = new List<PictureBox>();
+        private PictureBox picA;
+        private PictureBox picB;
+        
+        bool gameOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPictures();
+        }
+
+        private void lStratus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void LoadPictures()
+        {
+            int leftPos = 20;
+            int topPos = 20;
+            int rows = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.Aqua;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+
+                if (rows < 4 )
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60; 
+                    
+                }
+                if (rows == 4)
+                {
+                    rows = 0;
+                    leftPos = 20;
+                    topPos = topPos + 60;
+                }
+            }
+
+            RestartGame();
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                return;
+            }
+            if (firstClicked == null)
+            {
+               picA = sender as PictureBox;
+               if (picA.Tag != null && picA.Image == null)
+               {
+                    picA.Image = Image.FromFile("pictures/" + (string)picA.Tag + ".png");
+                    firstClicked = (string)picA.Tag;
+
+                }
+                
+            }
+            else if ( secondClicked == null)
+            {
+                picB = sender as PictureBox;
+
+                if (picB.Tag != null && picB.Image == null)
+                {
+                    picB.Image = Image.FromFile("pictures/" + (string)picB.Tag + ".png");
+                    secondClicked = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckForMatch(picA, picB);
+            }
+        }
+
+        private void RestartGame()
+        {
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+
+            gameOver = false;
+        }
+
+        private void CheckForMatch(PictureBox A, PictureBox B)
+        {
+            if (firstClicked == secondClicked)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+           
+            firstClicked = null;
+            secondClicked = null;
+
+            foreach (PictureBox pics in pictures)
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+               GameOver("GG you won , Try again if you want ");
+            }
+
+        }
+
+
+        private void GameOver(string msg)
+        {
+            GameTimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg);
+        }
+    }
+}
